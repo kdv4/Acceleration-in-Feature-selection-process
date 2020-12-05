e001590350af2ac2d43fbe572dbb4823becaaee8
kdv4 <vaishnanikishanv1@gmail.com>
2020-12-05 15:59:46 +0530

    Update serial.py

--- serial.py
+++ serial.py
@@ -4,6 +4,7 @@ import pandas as pd
 import serial_support as ss
 from sklearn.model_selection import train_test_split
 import math
+from time import time
 import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning) 
 
@@ -14,7 +15,6 @@ file='dataset.csv'
 text_indices=[1]
 start=2
 out='1'
-delimiter=';'
 
 if __name__ == "__main__":
     dataset=pd.read_csv(file)
@@ -38,31 +38,35 @@ if __name__ == "__main__":
         Y_raw= dataset.loc[:,out]
         # category.insert(0,out)   
 
+    start=time()
     X_train, X_test, Y_train, Y_test = train_test_split(X_raw, Y_raw, test_size = 0.2, random_state = 0)
     print("Accuracy of Raw data is: "+str(ss.navie_byes(X_train,Y_train,X_test,Y_test)))
-    
+    end=time()
+    print("Time taken by it: "+str((end-start)*1000)+" ms")
 
     #Direct Feature selection
+    start=time()
     selected_features = cr.cal_vif(X_raw) 
     X_train, X_test, Y_train, Y_test = train_test_split(selected_features, Y_raw, test_size = 0.2, random_state = 0)
     print("Accuracy using direct feature selection is: "+str(ss.navie_byes(X_train,Y_train,X_test,Y_test)))
-    
+    end=time()
+    print("Time taken by it: "+str((end-start)*1000)+" ms")
     
     #Kmeans+Feature Selection
-    
-    start=int(math.sqrt(len(dataset)/2))
-    ss.check_kmenas(X_raw,Y_raw,start,start*2+5,category)
-
-    centroids=kmeans.kmeansCluster(X_raw,18)
+    # start=int(math.sqrt(len(dataset)/2))
+    # ss.check_kmenas(X_raw,Y_raw,start,start*2+5,category)
+    start=time()
+    centroids=kmeans.kmeansCluster(X_raw,19)
     ss.Write_XL(centroids,len(centroids),len(centroids[0]),category)
     
     data_kmeans=pd.read_excel('centroids.xls')
     X_kmeans=data_kmeans.iloc[:,:] 
     
     selected_features = cr.cal_vif(X_kmeans)
-
     X_modify=X_raw.loc[:,selected_features.columns]
 
     X_train, X_test, Y_train, Y_test = train_test_split(X_modify, Y_raw, test_size = 0.2, random_state = 0)
     print("Accuracy using Kmenas+feature selection is: "+str(ss.navie_byes(X_train,Y_train,X_test,Y_test)))
-    
+
+    end=time()
+    print("Time taken by it: "+str((end-start)*1000)+" ms")
